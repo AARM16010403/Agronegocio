df032c57f3047f4cd0288f34608e89d3feaa1b1f
Luis Angel <aruiz403@accitesz.com>
2020-02-16 22:44:29 -0600

    Tablas

--- Datoss/Conexion.py
+++ Datoss/Conexion.py
@@ -3,7 +3,7 @@ import pyodbc
 class Conexion:
     db=None
     def __init__(self):
-        self.db= pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='
+        self.db = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='
                                 +'DESKTOP-G0GC4F8'
                                 +';DATABASE='
                                 +'ERP2020'+';UID='+'DBA'+';PWD='+'qwerty')
--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, request, url_for,session
+from flask import Flask, render_template, request, url_for, session
 from werkzeug.utils import redirect
+
+from Datoss.ClientesDAO import ClientesDAO
+from Datoss.CultivosDAO import CultivosDAO
+from Datoss.AsociacionesDAO import AsociacionesDAO
+from Datoss.UnidadesTransDAO import UnidadesTransDAO
 import json
 
 app=Flask(__name__)
@@ -19,7 +24,26 @@ def login():
             return render_template('index.html')
     else:
         return render_template('index.html')
-
+@app.route('/cultivos')
+def cultivos():
+    cdao = CultivosDAO()
+    lista = cdao.obtenerCultivos()
+    return render_template('Ventas/Cultivos.html', cultivos=lista, user=session.get('user'))
+@app.route('/asociacion')
+def asociaciones():
+    adao = AsociacionesDAO()
+    lista = adao.obtenerAsociaciones()
+    return render_template('Ventas/Asociaciones.html', asociaciones=lista, user=session.get('user'))
+@app.route('/unidades')
+def unidades():
+    udao = UnidadesTransDAO()
+    lista = udao.obtenerUnidades()
+    return render_template('Ventas/UnidadesTrans.html', unidades=lista, user=session.get('user'))
+@app.route('/clientes')
+def clientes():
+    cdao = ClientesDAO()
+    lista = cdao.obtenerClientes()
+    return render_template('Ventas/Clientes.html', clientes=lista, user=session.get('user'))
 
 if __name__=='__main__':
     app.run(debug=True)
